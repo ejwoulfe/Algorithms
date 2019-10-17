5ea04984d3c71803f204bb4a96b7bfcc80cf1926
Ed Woulfe <edwoulfe@gmail.com>
2019-10-17 16:59:37 -0500

    Update freeCodeCampAlgorithms.js

--- freeCodeCampAlgorithms.js
+++ freeCodeCampAlgorithms.js
@@ -423,3 +423,55 @@ function orbitalPeriod(arr) {
 }
 
 orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]);
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+const roman = {
+  1: "I",
+  2: "II",
+  3: "III",
+  4: "IV",
+  5: "V",
+  6: "VI",
+  7: "VII",
+  8: "VIII",
+  9: "IX",
+  10: "X",
+  20: "XX",
+  30: "XXX",
+  40: "XL",
+  50: "L",
+  60: "LX",
+  70: "LXX",
+  80: "LXXX",
+  90: "XC",
+  100: "C",
+  200: "CC",
+  300: "CCC",
+  400: "CD",
+  500: "D",
+  600: "DC",
+  700: "DCC",
+  800: "DCCC",
+  900: "CM",
+  1000: "M",
+  2000: "MM",
+  3000: "MMM"
+}
+function convertToRoman(num) {
+  let numberString = num.toString();
+  let resultArr = [];
+
+  for (let i = 0; i < numberString.length; i++) {
+    let letter = numberString[i];
+    for (let z = numberString.length - 1; z > i; z--) {
+      letter += "0";
+
+    }
+    resultArr.push(roman[letter])
+
+  }
+  return resultArr.join("")
+}
+
+console.log(convertToRoman(329));
